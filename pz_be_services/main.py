77251824acb7fe4591a0289a746e29aef5901bfe
Bidipto Dey <bidipto.7.kmr@gmail.com>
2025-07-04 01:25:10 +0530

    🔧 Add CORS middleware configuration for improved API access control

--- pz_be_services/main.py
+++ pz_be_services/main.py
@@ -2,12 +2,21 @@ from fastapi import FastAPI
 from routers.v1 import health_router
 from routers.v1 import user_router
 from core.logger import get_logger
+from fastapi.middleware.cors import CORSMiddleware
 
 
 logger = get_logger(__name__)
 logger.info("app starting")
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 app.include_router(health_router, prefix="/v1")
 app.include_router(user_router, prefix="/v1/user")
