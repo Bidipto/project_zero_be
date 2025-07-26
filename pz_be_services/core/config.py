c0cefa907d2c66b5436800e754edf019632cffec
Ace <acextreme98@gmail.com>
2025-07-26 23:35:47 +0530

    github

--- pz_be_services/core/config.py
+++ pz_be_services/core/config.py
@@ -7,5 +7,5 @@ class EnvironmentVariables:
 
    GITHUB_CLIENT_ID=os.getenv("your_client_id","Ov23limQuQTxOfy9RmkL")
    GITHUB_CLIENT_SECRET=os.getenv("your_client_secret","49422604296bd5a04612ec2ab4c5746a910feb1d")
-   GITHUB_REDIRECT_URI="http://localhost:8000/login/github/callback"
+   
 
